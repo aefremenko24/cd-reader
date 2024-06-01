9becc78c5b6d6e679a3a04f92f510c73ee187001
Arthur Efremenko <aefremenko24@gmail.com>
2024-05-31 22:41:19 -0700

    Initial commit

---
+++ cd-reader/cd_readerApp.swift
@@ -0,0 +1,17 @@
+//
+//  cd_readerApp.swift
+//  cd-reader
+//
+//  Created by Arthur Efremenko on 5/31/24.
+//
+
+import SwiftUI
+
+@main
+struct cd_readerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
