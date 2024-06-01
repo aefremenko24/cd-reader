5f7af9a87746d99f6d59a178628d5ec59b626f58
Arthur Efremenko <aefremenko24@gmail.com>
2024-05-31 23:44:19 -0700

    Added scan barcode functionality for .qr, .upce, .ean8, .ean13 types using CodeScanner library.

--- cd-reader/ContentView.swift
+++ cd-reader/ContentView.swift
@@ -6,16 +6,36 @@
 //
 
 import SwiftUI
+import CodeScanner
 
 struct ContentView: View {
+    @State var isPresentingScanner = false
+    @State var scannedCode: String = "Scan a barcode to get started."
+    
+    var scannerSheet : some View {
+        CodeScannerView(
+            codeTypes: [.qr, .upce, .ean8, .ean13],
+            completion: { result in
+                if case let .success(code) = result {
+                    self.scannedCode = code.string
+                    self.isPresentingScanner = false
+                }
+            }
+        )
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 10) {
+            Text(scannedCode)
+            
+            Button("Scan barcode") {
+                self.isPresentingScanner = true
+            }
+            
+            .sheet(isPresented: $isPresentingScanner) {
+                self.scannerSheet
+            }
         }
-        .padding()
     }
 }
 
